a06fa6e24490b26c09d8cf7f4069d48be7226d09
Dani Matton <danielle@Danis-MacBook-Pro.local>
2020-03-24 15:05:18 -0600

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ recipe1 = Recipe.create(
     likes: 4)
 recipe2 = Recipe.create(
     title: "Calendula Hand Cream", 
-    image: "https://lh3.googleusercontent.com/proxy/qWJBtHYrjgwKGfKVxlH-A3NwE_29FI1GrDM1xdFVbk6vhYPwqt1Z7KaDSwAX1tYewI4MMHxGI4yGb6HH8dd4AfaZ6YiWCoq3KBL9nKalTBRrnPdQjsTbP9w1PNaJrkivhHzXqAuYpTHusnARVysbIQIWuox5cO4_0LI8my9YHdju6y35gkWT", 
+    image: "https://www.lgbotanicals.com/assets/images/Calendula-736c.jpg", 
     ingredients: "350 grams herbal water extraction, such as strong Calendula tea (see Step 1) 76 grams oil of your choice, such as Calendula oil 25 grams shea butter 15 grams cetyl alcohol 25 grams emulsifying wax 2.5 grams Liquid Germall Plus 5 grams essential oils of your choice (optional)", 
     instructions: "If using an herbal water extraction, prepare it now via your preferred method. I infused a handful of dried Calendula officinalis flowers in hot distilled water for 20 minutes, covered. Strain. Using a scale, weigh out the Calendula tea, oil, shea butter, cetyl alcohol, and emulsifying wax in a sterilized heat-proof jar. Using a water bath, heat the mixture in the jar to 170°F. Hold at that temperature for 20 minutes. Be careful not to overheat the oils. Use a thermometer to keep track of the temperature. Remove the jar from the water bath, and let cool. When 160°F is reached, use an immersion blender to blend the mixture until homogeneous. Do not overblend. When the temperature drops to 110°F, add Liquid Germall Plus and essential oils. Transfer to a lidded storage container. Allow to cool completely before putting the lid on. Store the Calendula lotion in a dark, cool place.", 
     notes: "Love it!",
@@ -49,25 +49,9 @@ recipe6 = Recipe.create(
     favorite: false,
     likes: 9)
 recipe7 = Recipe.create(
-    title: "Poppy Tincture", 
-    image: "https://cff2.earth.com/uploads/2019/02/10121232/Poppy-flowers-are-bright-and-splashy-%E2%80%93-heres-why.jpg", 
-    ingredients: "Poppy flower, poppy seeds, grain-alcohol", 
-    instructions: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
-    notes: "Love it!",
-    favorite: false,
-    likes: 4)
-recipe8 = Recipe.create(
     title: "Calming After Dinner Tea", 
     image: "https://www.botanicalartandartists.com/uploads/4/6/6/0/46602337/lilium-candidum-monstrosum_orig.jpg", 
     ingredients: "Chamomile, lemon balm, catnip, fennel, lavender, spearmint, rosehips", 
-    instructions: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
-    notes: "Love it!",
-    favorite: false,
-    likes: 4)
-recipe9 = Recipe.create(
-    title: "Hibiscus Syrup", 
-    image: "https://www.botanicalartandartists.com/uploads/4/6/6/0/46602337/primula-ladybird-and-butterfly_orig.jpg", 
-    ingredients: "3 things", 
     instructions: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
     notes: "Love it!",
     favorite: false,
